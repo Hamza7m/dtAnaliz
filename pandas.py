194809d0131a9dcbef41392cf00dfcaef8d7e0ae
Hamza Almahmoud <101905387+Hamza7m@users.noreply.github.com>
2023-12-02 21:31:46 +0300

    update to branch2

---
+++ pandas.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+print("wellcome from pandas2 ")
+
+list = [ 2,3,45,3,5,3,2,2,4,5]
+
+cl1 = ["hamza" , "idrissa" , 242454 , 656557 , 67 , 'B']
+
+cl2 = ["Ali" , "idrissa" , 75653 , 82992 , 27 , 'C']
+
+cl3 = ["Fatima" , "idrissa" , 797755 , 292992 , 13 , 'B']
+
+cl4 = ["Lara" , "idrissa" , 33332 , 34343 , 35 , 'A']
+
+cl5 = ["Lama" , "idrissa" , 3536 , 634563 , 26 , 'A']
+frame = pd.DataFrame([cl1,cl2])
